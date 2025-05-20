07af883d1bf3945c5c43d83ef29feed13f1ae967
whycw010 <wahyucahyo2014@mail.ugm.ac.id>
2025-05-20 15:06:42 +0700

    handle invalid jwt token verification

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -1,35 +1,35 @@
-import {User,Role,UserData} from '../model/User'
-import {IUserRepository} from '../repository/interface/IUserRepository'
+import { User, Role, UserData } from '../model/User'
+import { IUserRepository } from '../repository/interface/IUserRepository'
 import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import {userRepository} from '../repository/repoExporter'
-import {readFileSync} from 'fs'
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import { userRepository } from '../repository/repoExporter'
+import { readFileSync } from 'fs'
 
-class AuthService{
+class AuthService {
     private userRepository: IUserRepository
     private defaultImageBase64: string
 
-    constructor(ur: IUserRepository){
+    constructor(ur: IUserRepository) {
         this.userRepository = ur
-        const defaultImage = readFileSync(__dirname + '/../../assets/user-default.webp', {encoding: 'base64'})
+        const defaultImage = readFileSync(__dirname + '/../../assets/user-default.webp', { encoding: 'base64' })
         this.defaultImageBase64 = `data:image/webp;base64,${defaultImage}`
     }
-    async register(username: string, email: string, password: string): Promise<UserData|null>{
+    async register(username: string, email: string, password: string): Promise<UserData | null> {
         const hash = await bcrypt.hash(password, 10)
         let newUser = new User(
             username,
             email,
             this.defaultImageBase64,
             hash,
-            Role.User 
+            Role.User
         )
-        const res =  await this.userRepository.create(newUser)
-        return res?.getData()||null
+        const res = await this.userRepository.create(newUser)
+        return res?.getData() || null
     }
-    async login(username: string, password: string): Promise<string|null>{
+    async login(username: string, password: string): Promise<string | null> {
         const res = await this.userRepository.getById(username)
         const passwordMatch = bcrypt.compareSync(password, String(res?.passwordHash))
-        if (res === null||!passwordMatch){
+        if (res === null || !passwordMatch) {
             return null
         }
         const token = jwt.sign(
@@ -44,16 +44,18 @@ class AuthService{
         )
         return token
     }
-    async authenticate(token: string): Promise<UserData|null>{
-        const decodedToken = jwt.verify(token, String(process.env.JWT_SECRET))
-        if (decodedToken === null || typeof decodedToken === 'string'){
+    async authenticate(token: string): Promise<UserData | null> {
+        let decodedToken: JwtPayload
+        try {
+            decodedToken = jwt.verify(token, String(process.env.JWT_SECRET)) as JwtPayload
+        } catch (error) {
             return null
         }
         return {
             username: decodedToken.username,
-            role: decodedToken.role 
+            role: decodedToken.role
         }
-    } 
+    }
 }
 const authService = new AuthService(userRepository)
 export default authService
