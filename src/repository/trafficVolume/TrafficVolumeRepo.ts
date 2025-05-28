7a77955efd365a9654ddfc176d51fc3237518f39
whycw010 <wahyucahyo2014@mail.ugm.ac.id>
2025-05-29 01:01:03 +0700

    implemented repo logic for traf endpoint

--- src/repository/trafficVolume/TrafficVolumeRepo.ts
+++ src/repository/trafficVolume/TrafficVolumeRepo.ts
@@ -4,42 +4,42 @@ import { ITrafficVolumeRepository } from "../interface/ITrafficVolumeRepository"
 import strftime from "strftime";
 
 const trafficDataSchema = {
-  "$jsonSchema": {
-    "bsonType": "object",
-    "required": [ "congestion_level", "datetime", "location"],
-    "properties": {
-      "congestion_level": {
-        "bsonType": "int"
-      },
-      "datetime": {
-        "bsonType": "string"
-      },
-      "location": {
-        "bsonType": "string"
-      },
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": ["congestion_level", "datetime", "location"],
+        "properties": {
+            "congestion_level": {
+                "bsonType": "int"
+            },
+            "datetime": {
+                "bsonType": "string"
+            },
+            "location": {
+                "bsonType": "string"
+            },
+        }
     }
-  }
 }
 const trafficPredSchema = {
-  "$jsonSchema": {
-    "bsonType": "object",
-    "required": [
-      "congestionLevelPredicted",
-      "datetime",
-      "location",
-    ],
-    "properties": {
-      "congestionLevelPredicted": {
-        "bsonType": "int"
-      },
-      "datetime": {
-        "bsonType": "string"
-      },
-      "location": {
-        "bsonType": "string"
-      },
+    "$jsonSchema": {
+        "bsonType": "object",
+        "required": [
+            "congestionLevelPredicted",
+            "datetime",
+            "location",
+        ],
+        "properties": {
+            "congestionLevelPredicted": {
+                "bsonType": "int"
+            },
+            "datetime": {
+                "bsonType": "string"
+            },
+            "location": {
+                "bsonType": "string"
+            },
+        }
     }
-  }
 }
 export class TrafficVolumeRepository implements ITrafficVolumeRepository {
     private db: Db
@@ -54,7 +54,7 @@ export class TrafficVolumeRepository implements ITrafficVolumeRepository {
         this.db = new MongoClient(url).db(dbName)
         this.ensureCollection()
     }
-    private async ensureCollection(): Promise<void>{
+    private async ensureCollection(): Promise<void> {
         this.trafficData = this.db.collection('trafficdata')
         this.trafficPrediction = this.db.collection('prediction')
     }
@@ -62,12 +62,18 @@ export class TrafficVolumeRepository implements ITrafficVolumeRepository {
     async getTrafficVolumeRange(locationId: string, timestampBegin: string, timestampEnd: string): Promise<TrafficVolume[]> {
         let volumeRange: TrafficVolume[] = []
 
-        let datetime = 2025051319
-        for (var i = 0; i <= 23; i++) {
-            const res = await this.trafficData.findOne({'location':'Tugu Jogja','datetime':'2025051319'})
-            console.log(res)
-            const td =  new TrafficVolume(res?.datetime, res?.location, res?.congestion_level, false)
-            volumeRange.push(td)
+        const res = await this.trafficData.find({ 'location': locationId, 'datetime': { $gt: timestampBegin, $lt: timestampEnd } }).toArray()
+        for (var data of res) {
+            const tv = new TrafficVolume(data.datetime, data.location, data.congestion_level, false)
+            volumeRange.push(tv)
+        }
+        console.log(volumeRange[volumeRange.length-1].datetime)
+        if (Number(volumeRange[volumeRange.length-1].datetime) < Number(timestampEnd)) {
+            const res = await this.trafficPrediction.find({ 'location': locationId, 'datetime': { $gt: volumeRange[volumeRange.length-1].datetime, $lt: timestampEnd } }).toArray()
+            for (var data of res) {
+                const tv = new TrafficVolume(data.datetime, data.location, data.congestionLevelPredicted, true)
+                volumeRange.push(tv)
+            }
         }
         return volumeRange
     }
