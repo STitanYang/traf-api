8aa5ebaa82c1719096d97ff64e7b1e24467677f1
Sean Titan Yang <seantitanyang2006@mail.ugm.ac.id>
2025-05-30 05:29:24 +0700

    replica wahyu directory

--- src/repository/trafficVolume/TrafficVolumeRepo.ts
+++ src/repository/trafficVolume/TrafficVolumeRepo.ts
@@ -21,18 +21,49 @@ export class TrafficVolumeRepository implements ITrafficVolumeRepository {
     }
 
     async getTrafficVolumeRange(locationId: string, timestampBegin: string, timestampEnd: string): Promise<TrafficVolume[]> {
-        let volumeRange: TrafficVolume[] = []
+        let volumeRange: TrafficVolume[] = [];
 
-        const res = await this.trafficData.find({ 'location': locationId, 'datetime': { $gt: timestampBegin, $lt: timestampEnd } }).toArray()
-        for (var data of res) {
-            const tv = new TrafficVolume(data.datetime, data.location, data.congestion_level, false)
-            volumeRange.push(tv)
+// 1. Ambil data historis lalu lintas
+        const historicalRes = await this.trafficData.find({ 
+            'location': locationId, 
+            'datetime': { $gte: timestampBegin, $lte: timestampEnd } 
+        }).toArray();
+
+        for (const data of historicalRes) {
+            const tv = new TrafficVolume(data.datetime, data.location, data.congestion_level, false);
+            volumeRange.push(tv);
         }
-        if (Number(volumeRange[volumeRange.length - 1].datetime) < Number(timestampEnd)) {
-            const res = await this.trafficPrediction.find({ 'location': locationId, 'datetime': { $gt: volumeRange[volumeRange.length - 1].datetime, $lt: timestampEnd } }).toArray()
-            for (var data of res) {
-                const tv = new TrafficVolume(data.datetime, data.location, data.congestionLevelPredicted, true)
-                volumeRange.push(tv)
+
+        // 2. Tentukan apakah data prediksi diperlukan dan dari kapan harus diambil
+        let shouldFetchPredictions = false;
+        let predictionQueryStartTime; // Waktu mulai untuk query data prediksi
+
+        if (volumeRange.length > 0) {
+            // Data historis ditemukan
+            const lastHistoricalDatetime = volumeRange[volumeRange.length - 1].datetime;
+            // Periksa apakah data historis terakhir kurang dari timestampEnd
+            if (Number(lastHistoricalDatetime) < Number(timestampEnd)) {
+                shouldFetchPredictions = true;
+                // Mulai prediksi dari waktu data historis terakhir
+                predictionQueryStartTime = lastHistoricalDatetime; 
+            }
+        } else {
+            // Tidak ada data historis ditemukan.
+            // Maka, kita perlu mengambil data prediksi dari awal rentang waktu yang diminta (timestampBegin).
+            shouldFetchPredictions = true;
+            predictionQueryStartTime = timestampBegin;
+        }
+
+        // 3. Jika data prediksi diperlukan, ambil datanya
+        if (shouldFetchPredictions) {
+            const predictionRes = await this.trafficPrediction.find({ 
+                'location': locationId, 
+                'datetime': { $gte: predictionQueryStartTime, $lte: timestampEnd } 
+            }).toArray();
+
+            for (const data of predictionRes) {
+                const tv = new TrafficVolume(data.datetime, data.location, data.congestionLevelPredicted, true);
+                volumeRange.push(tv);
             }
         }
         return volumeRange
